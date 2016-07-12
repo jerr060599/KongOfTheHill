f7e297471c51b6085d23e2df4b63d1cbc7927d65
jerry060599 <jerry060599@gmail.com>
2016-07-12 14:31:05 -0400

    Checkpoint script updated

--- Assets/scripts/Checkpoint.cs
+++ Assets/scripts/Checkpoint.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Checkpoint : Activatable
 {
 	public Sprite blue, orange, neutral;
+	public GameObject blueLight, orangeLight;
 	Checkpoint tmp;
 	CharControl curOwner = null;
 
@@ -14,6 +15,9 @@ public class Checkpoint : Activatable
 		if (curOwner != null)
 			curOwner.curSpawn = curOwner.baseSpawn;
 		curOwner = player;
+		orangeLight.SetActive (false);
+		blueLight.SetActive (false);
+		(player.player == 0 ? orangeLight : blueLight).SetActive (true);
 		sr.sprite = player.player == 0 ? orange : blue;
 		player.curSpawn = gameObject;
 	}
@@ -21,5 +25,7 @@ public class Checkpoint : Activatable
 	public void disown ()
 	{
 		sr.sprite = neutral;
+		orangeLight.SetActive (false);
+		blueLight.SetActive (false);
 	}
 }
