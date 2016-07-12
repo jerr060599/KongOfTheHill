404d9a97f29473a51b6d46b6869db69ccc774817
jerry060599 <jerry060599@gmail.com>
2016-07-12 14:17:53 -0400

    Checkpoint animations!!

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
-	public GameObject c0, c1;
+	public GameObject c0, c1, bg;
 	float sizeScale = 0.5f;
 	Vector3 shakePos = Vector3.zero, curPos;
 	Camera cam;
@@ -21,6 +21,7 @@ public class CameraMovement : MonoBehaviour
 		cam.transform.position = (curPos += ((c0.transform.position + c1.transform.position) / 2f - curPos) * 0.1f) + shakePos;
 		shakePos *= 0.7f;
 		cam.orthographicSize += (Mathf.Max (64f, (c0.transform.position - c1.transform.position).magnitude * sizeScale) - (float)cam.orthographicSize) * 0.1f;
+		bg.transform.localScale = new Vector3 (cam.orthographicSize, cam.orthographicSize, 1f) / 64f;
 	}
 
 	public void shake ()
