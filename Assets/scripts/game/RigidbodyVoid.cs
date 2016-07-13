07c5701e83bc600f517c7f902b9f636a0250d2bc
jerry060599 <jerry060599@gmail.com>
2016-07-13 18:48:54 -0400

    Roll control

---
+++ Assets/scripts/game/RigidbodyVoid.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidbodyVoid : MonoBehaviour
+{
+	public void OnTriggerEnter2D (Collider2D c)
+	{
+		if (c.attachedRigidbody != null)
+		if (c.attachedRigidbody.gameObject.GetComponent<CharControl> () == null)
+			Destroy (c.gameObject);
+	}
+}
---
+++ Assets/scripts/game/RollRedirect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollRedirect : MonoBehaviour
+{
+	public float scaler = 1f;
+
+	public void OnTriggerEnter2D (Collider2D c)
+	{
+		ConstantRoll cr;
+		if ((cr = c.gameObject.GetComponent<ConstantRoll> ()) != null)
+			cr.w *= scaler;
+	}
+}
