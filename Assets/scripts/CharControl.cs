287e4036329ac8dde3f26fa264120ef9e26d5085
jerry060599 <jerry060599@gmail.com>
2016-07-13 00:04:40 -0400

    Fixed animation

--- Assets/scripts/CharControl.cs
+++ Assets/scripts/CharControl.cs
@@ -107,16 +107,17 @@ public class CharControl : MonoBehaviour
 			pysc.AddForce (new Vector2 (0, jumpF));
 			jumpTime = jumpCD;
 		}
-		sr.sprite = variate ? walk0 : walk1;
 		if (onLadder && Mathf.Abs (lastJuicePos.y - pysc.position.y) > pixelPerSound) {
 			SoundManager.script.playOnListener (variate ? SoundManager.script.climb0 : SoundManager.script.climb1);
 			lastJuicePos = pysc.position;
 			variate = !variate;
 		} else if (canWalk && (lastJuicePos - pysc.position).sqrMagnitude > pixelPerSound * pixelPerSound) {
+			sr.sprite = variate ? walk0 : walk1;
 			SoundManager.script.playOnListener (variate ? SoundManager.script.walk0 : SoundManager.script.walk1, 0.25f);
 			lastJuicePos = pysc.position;
 			variate = !variate;
-		}
+		} else if (!onLadder)
+			sr.sprite = variate ? walk0 : walk1;
 		if (!canWalk && !onLadder)
 			sr.sprite = pysc.velocity.y > 0 ? fallUp : fallDown;
 		onLadder = false;
