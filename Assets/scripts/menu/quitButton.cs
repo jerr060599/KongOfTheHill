0cc5e6dd42e7733ed8a87fd7067265b364da4e97
jerry060599 <jerry060599@gmail.com>
2016-07-14 16:00:44 -0400

    Imported tutorial, main menu

---
+++ Assets/scripts/menu/quitButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class quitButton : MonoBehaviour {
+	GameObject thisQuitButton;
+	UnityEngine.UI.Button quit_button;
+
+	void Quit() {
+		Application.Quit ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		thisQuitButton = GameObject.FindWithTag ("menuQuitButton");
+		quit_button = GetComponent<UnityEngine.UI.Button> ();
+		quit_button.onClick.AddListener (Quit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
