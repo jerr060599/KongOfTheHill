33099043bc46750228f8b523480a194c97e1e41c
jerry060599 <jerry060599@gmail.com>
2016-07-14 10:52:47 -0400

    Radio group

---
+++ Assets/scripts/game/RadioGroup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RadioGroup : MonoBehaviour
+{
+	List<Activatable> list = new List<Activatable> ();
+
+	public void register (Activatable a)
+	{
+		list.Add (a);
+	}
+
+	public void checkout (Activatable a, CharControl player)
+	{
+		foreach (Activatable b in list)
+			if (b.activated && b != a)
+				b.activate (player);
+	}
+}
